18343e65975ae245b0af8bb1242111117a2ee7ff
Your Name <you@example.com>
2020-06-21 22:32:21 +0300

    done bottom navigation

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -1,10 +1,14 @@
+import React from 'react'
 import {createAppContainer} from 'react-navigation'
 import {createStackNavigator} from 'react-navigation-stack'
+import {createBottomTabNavigator} from 'react-navigation-tabs'
 import {Platform} from 'react-native'
+import {Ionicons} from '@expo/vector-icons'
 
 import { MainScreen } from '../screens/MainScreen'
 import { PostScreen } from '../screens/PostScreen'
 import { CreateScreen } from '../screens/CreateScreen'
+import { BookedScreen } from '../screens/BookedScreen'
 import { THEME } from '../theme'
 
 const PostNavigation = createStackNavigator({
@@ -23,4 +27,36 @@ const PostNavigation = createStackNavigator({
     }
 })
 
-export const AppNavigation = createAppContainer(PostNavigation)
+const BookedNavigation = createStackNavigator({
+    Booked: BookedScreen,
+    Post:  PostScreen,
+    Create: CreateScreen
+}, {
+    defaultNavigationOptions: {
+        headerStyle: {
+            backgroundColor: Platform.OS === "android" ? THEME.MAIN_COLOR : '#fff'
+        },
+        headerTintColor: Platform.OS === "android" ? '#fff' : THEME.MAIN_COLOR
+    }
+})
+
+const MainNavigation = createBottomTabNavigator({
+    Post: {
+        screen: PostNavigation,
+        navigationOptions: {
+            tabBarIcon: info => (<Ionicons name="ios-albums" size={25} color={info.tintColor}/>)
+        }
+    },
+    Booked: {
+        screen: BookedNavigation,
+        navigationOptions: {
+            tabBarIcon: info => (<Ionicons name="ios-bookmark" size={25} color={info.tintColor}/>)
+        }
+    },
+}, {
+    tabBarOptions: {
+        activeTintColor: THEME.MAIN_COLOR
+    }
+})
+
+export const AppNavigation = createAppContainer(MainNavigation)
--- src/theme.js
+++ src/theme.js
@@ -1,5 +1,5 @@
 export const THEME = {
-    MAIN_COLOR: '#99ab65',
+    MAIN_COLOR: '#19ab65',
     DANGER_COLOR: '#ff1815',
     EDIT_COLOR: '#1f8615'
 }
